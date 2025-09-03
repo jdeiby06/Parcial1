6904e3d528f86569a644d85fab48b25de0829cd4
jdeiby06 <jdeibylopez@estudiante.uniajc.edu.co>
2025-09-02 19:21:32 -0500

    Se cumplio el primer punto

--- Libro.java
+++ Libro.java
@@ -4,5 +4,57 @@ public class Libro {
     protected String autor;
     protected int numEjemplares;
     protected int numPrestados;
+        // Constructor vacio por defecto
+    public Libro() {
+        this.titulo = "";
+        this.autor = "";
+        this.numEjemplares = 0;
+        this.numPrestados = 0;
+    }
 
+    // Constructor con parámetros
+    public Libro(String titulo, String autor, int numEjemplares, int numPrestados) {
+        this.titulo = titulo;
+        this.autor = autor;
+        this.numEjemplares = 2;
+        this.numPrestados = 1;
+    }
+
+    // Métodos get y set
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getAutor() {
+        return autor;
+    }
+
+    public void setAutor(String autor) {
+        this.autor = autor;
+    }
+
+    public int getNumEjemplares() {
+        return numEjemplares;
+    }
+
+    public void setNumEjemplares(int numEjemplares) {
+        this.numEjemplares = numEjemplares;
+    }
+
+    public int getNumPrestados() {
+        return numPrestados;
+    }
+
+    public void setNumPrestados(int numPrestados) {
+        this.numPrestados = numPrestados;
+    }
 }
+
+    
+
+    
+
